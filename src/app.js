fa35594157fc599026da939a7d94f389906f90f0
Vincent Zerbib <contact@zelabstudio.com>
2024-01-26 10:10:33 +0100

    save work

--- src/app.js
+++ src/app.js
@@ -5,33 +5,52 @@ const cors = require('cors');
 const Sequelize = require('sequelize');
 const config = require('./config/config.js')[process.env.NODE_ENV || 'development'];
 const router = require('./routes/index.js');
+const { ApolloServer } = require('@apollo/server');
+const { ExpressMiddleware } = require('@apollo/server/express4');
 
-app.use(cors())
-app.options(process.env.FRONTEND_URL, cors());
+const initApplication = async() => {
+    app.use(cors())
+    app.options(process.env.FRONTEND_URL, cors());
 
-const sequelize = new Sequelize(config.database, config.username, config.password, {
-    port: config.port,
-    host: config.host,
-    dialect: config.dialect,
-    dialectOptions: {
-        connectTimeout: 60000
-    }
-});
-
-sequelize.sync()
-    .then(() => {
-        console.log('database synchronised');
-    })
-    .catch(err => {
-        console.error('database synchronisation error :', err);
+    const sequelize = new Sequelize(config.database, config.username, config.password, {
+        port: config.port,
+        host: config.host,
+        dialect: config.dialect,
+        dialectOptions: {
+            connectTimeout: 60000
+        }
     });
 
-app.get("/", (req, res) => {
-    res.send("Welcome to my API");
-})
+    sequelize.sync()
+        .then(() => {
+            console.log('database synchronised');
+        })
+        .catch(err => {
+            console.error('database synchronisation error :', err);
+        });
 
-app.use("/api", router);
+    app.get("/", (req, res) => {
+        res.send("Welcome to my API");
+    })
 
-app.listen(process.env.PORT, () => {
-    console.log(`server launch on port ${process.env.PORT}`);
-});
+    app.use("/api", router);
+
+    const serverGraphQL = new ApolloServer({
+        //à passer : les types (typagesdes entités et des resolvers (controllers))
+        // Les resolvers : les fonctions du CRUD
+            // Query : GET / READ
+            // Mutation : POST / CREATE / PUT / UPDATE / DELETE
+    })
+
+    await serverGraphQL.start();
+
+    app.use(ExpressMiddleware(serverGraphQL, {
+        path: '/graphql'
+    }));
+
+    app.listen(process.env.PORT, () => {
+        console.log(`server launch on port ${process.env.PORT}`);
+    });
+}
+
+initApplication();
