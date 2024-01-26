50de4c77702c14b72efd9a23b83b76028fce9482
Vincent Zerbib <contact@zelabstudio.com>
2024-01-26 10:28:26 +0100

    save work

--- src/app.js
+++ src/app.js
@@ -6,11 +6,12 @@ const Sequelize = require('sequelize');
 const config = require('./config/config.js')[process.env.NODE_ENV || 'development'];
 const router = require('./routes/index.js');
 const { ApolloServer } = require('@apollo/server');
-const { ExpressMiddleware } = require('@apollo/server/express4');
+const { expressMiddleware } = require('@apollo/server/express4');
 
 const initApplication = async() => {
     app.use(cors())
     app.options(process.env.FRONTEND_URL, cors());
+    app.use(express.json());
 
     const sequelize = new Sequelize(config.database, config.username, config.password, {
         port: config.port,
@@ -35,16 +36,46 @@ const initApplication = async() => {
 
     app.use("/api", router);
 
+    const typeDefs = `
+        type Article {
+            id : ID!
+            title: String
+            description: String
+            date: String
+        }
+        type Query {
+            getArticles: [Article]!
+            getArticle(id: ID!): Article!
+        }
+    `;
+
+    const resolvers = {
+        Query: {
+            getArticles: (parent, args, context, info) => {
+                return [
+                    {
+                        id:1,
+                        title: "Article 1",
+                        description: "Description de l'article 1",
+                        date: "2021-01-01"
+                    }
+                ]
+            }
+        }
+    }
+
     const serverGraphQL = new ApolloServer({
         //à passer : les types (typagesdes entités et des resolvers (controllers))
         // Les resolvers : les fonctions du CRUD
             // Query : GET / READ
-            // Mutation : POST / CREATE / PUT / UPDATE / DELETE
+        // Mutation : POST / CREATE / PUT / UPDATE / DELETE
+        typeDefs,
+        resolvers
     })
 
     await serverGraphQL.start();
 
-    app.use(ExpressMiddleware(serverGraphQL, {
+    app.use(expressMiddleware(serverGraphQL, {
         path: '/graphql'
     }));
 
